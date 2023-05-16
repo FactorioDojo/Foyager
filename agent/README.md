f3ed17b5f9308a1aed32899b2eab0fd3b22a6e52
willjallen <wallen2268@gmail.com>
2023-05-15 23:43:15 -0500

    Gitignore, rename agent to server, add to readme in agent

---
+++ agent/README.md
@@ -0,0 +1,9 @@
+# Usage
+
+First open a headless factorio server using the following command
+`factorio.exe --start-server [save-name].zip --rcon-port [port] --rcon-password [password]`
+
+Then start the server
+`python main.py`
+
+(Todo: run in interactive mode w/ text interface)
--- agent/main.py
+++ agent/main.py
@@ -3,14 +3,14 @@ import json
 import openai
 
 from rcon.source import Client
-from helpers import process_initial_file
+# from helpers import process_initial_file
 
 from actions import Actions
 from writeouts import Writeouts
 
 
-init_file = "/opt/factorio/script-output/init_out.txt"
-initial_data_file = 'data/initial_data.json'
+# init_file = "/opt/factorio/script-output/init_out.txt"
+# initial_data_file = 'data/initial_data.json'
 
 
 actions = Actions(1)
