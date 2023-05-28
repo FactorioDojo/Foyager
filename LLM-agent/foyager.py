ddd0c2e360db62e2554e6bf24efa135100410709
willjallen <wallen2268@gmail.com>
2023-05-28 16:31:51 -0500

    Delete old agent file

--- LLM-agent/foyager.py
+++ LLM-agent/foyager.py
@@ -1,21 +1,3 @@
-from factorio_interface import FactorioInterface 
-from LLMagent import Agent
-
-
-'''
-Roughly: 
-
-- 'agent.py' uses 'guidance' to query LLM for instructions. 
-- If interactive_mode is enabled, then a human is able to modify each query to the LLM. If it is disabled we use an autonomous routine (for later)
-- These instructions are parsed and then sent as actions in `factorio.py`.
-- 'agent.py' then query 'factorio.py' for the response of the environment to the action, or for a writeout of the environment generally.
-'''
-
-
-
-agent = Agent(factorio_interface=FactorioInterface(1), interactive_mode=False)
-agent.run()
-
 
 
 import copy
@@ -25,7 +7,7 @@ import time
 from typing import Dict
 
 import voyager.utils as U
-from .env import VoyagerEnv
+from .env import FoyagerEnv
 
 from .agents import ActionAgent
 from .agents import CriticAgent
