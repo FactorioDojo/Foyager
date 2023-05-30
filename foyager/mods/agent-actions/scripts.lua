9c91b9150ecd4f1082048ecabce3fe1a1ed05326
willjallen <wallen2268@gmail.com>
2023-05-30 15:19:50 -0500

    More descriptive errors, append message ids for easier log scraping

--- foyager/mods/agent-actions/scripts.lua
+++ foyager/mods/agent-actions/scripts.lua
@@ -5,29 +5,32 @@ function print_player(msg)
     game.players[1].print(msg)
 end
 
-function print_log(msg)
-    log(msg)
+-- Print log to file with msg_id attached for easier scraping
+function id_log(msg_id, msg)
+    log("Message ID: " .. msg_id .. " " ..msg)
 end
 
 
 -- Loads in a script encoded in base64, saves it to the global context
-local function load_script(function_name, script)
+local function load_script(msg_id, function_name, script)
     -- Decode the script string
 	local script_string = base64.decode(script)
     local loaded_chunk, err_msg = loadstring(script_string)
     
     -- If there's an error during loading, log it and return
     if loaded_chunk == nil then
-        log("Error loading script: " .. err_msg)
+        id_log(msg_id, "Error loading script" .. function_name .. ": " .. err_msg)
         return
     end
+    
+    id_log(msg_id, "Script " .. function_name .. " loaded successfully")
 
     -- Try running the function and catching any runtime errors
     local ok, script_fn = pcall(loaded_chunk)
 
     -- If there's a runtime error, log it and return
     if not ok then
-        log("Error running script: " .. script_fn)  -- script_fn contains error message in this case
+        id_log(msg_id, "Error running script " .. function_name .. ": " .. script_fn)  -- script_fn contains error message in this case
         return
     end
 
@@ -35,14 +38,16 @@ local function load_script(function_name, script)
 end
 
 -- Executes a script
-local function execute_script(function_name, ...)
+local function execute_script(msg_id, function_name, ...)
     if _G[function_name] then
         local ok, err = pcall(_G[function_name], ...)
         if not ok then
-            log("Error executing script " .. function_name .. ": " .. err)
+            id_log(msg_id, "Error executing script " .. function_name .. ": " .. err)
+        else
+            id_log(msg_id, "Script " .. function_name .. " executed successfully")
         end
     else
-        log("Error: Function " .. function_name .. " does not exist.")
+        id_log(msg_id, "Error: Function " .. function_name .. " does not exist.")
     end
 end
 
