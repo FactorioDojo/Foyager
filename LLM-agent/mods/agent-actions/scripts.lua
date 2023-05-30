9ed52b4c053f4e82cc96840052b21a6bded3f1df
willjallen <wallen2268@gmail.com>
2023-05-29 21:07:38 -0500

    Script loader works

--- LLM-agent/mods/agent-actions/scripts.lua
+++ LLM-agent/mods/agent-actions/scripts.lua
@@ -1,22 +1,27 @@
-local base64lib = require ("base64")
+local base64 = require("base64")
 scripts = {}
 
+function DEBUG(msg)
+    game.players[1].print(msg)
+end
+
+
 -- Loads in a script encoded in base64, saves it to script table
-local function load_script(script, function_name)
-	script_string = base64lib.decode(script, base64lib.DEFAULT_DECODER, true)
-	script_function = loadstring(script_string)
-	scripts[function_name] = script_function
+local function load_script(function_name, script)
+	local script_string = base64.decode(script)
+	loaded_chunk = assert(loadstring(script_string))
+	script_fn = loaded_chunk()
+    scripts[function_name] = script_fn
 end
 
 -- Executes a script
-local function execute_script(function_name)
-	scripts[function_name]()
+local function execute_script(function_name, ...)
+    scripts[function_name](...)
 end
-      
-  remote.add_interface("scripts", {
+
+    remote.add_interface("scripts", {
     load_script=load_script,
-    execute_script=execute_script,
+    execute_script=execute_script
       })
 
-return scripts 
-
+return scripts
