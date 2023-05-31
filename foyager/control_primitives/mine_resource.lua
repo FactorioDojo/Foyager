6feeff0cbc3eef69fb7754ad0c0b5201185577ba
willjallen <wallen2268@gmail.com>
2023-05-30 20:43:32 -0500

    Change DEBUG() to clog()

--- foyager/control_primitives/mine_resource.lua
+++ foyager/control_primitives/mine_resource.lua
@@ -1,26 +1,26 @@
 function mine_resource(ore_type, quantity)
     -- Check if arguments are valid
     if type(ore_type) ~= 'string' or type(quantity) ~= 'number' then
-        DEBUG("Invalid arguments. 'ore_type' should be a string and 'quantity' should be a number.")
+        clog("Invalid arguments. 'ore_type' should be a string and 'quantity' should be a number.")
         return false
     end
 
+    local player = game.players[1]
+
     -- Check if the player is in a valid state
     if not player.character or player.character.mining_state.mining then
-        DEBUG("Player is not in a valid state for mining.")
+        clog("Player is not in a valid state for mining.")
         return false
     end
 
     -- Find nearest ore entity
     local position = player.character.position
     local surface = player.surface
-    local resource = surface.find_nearest_entity("resource", position, 1000, function(entity)
-        return entity.prototype.resource_category == 'basic-solid' and entity.name == ore_type
-    end)
+    local resource = find_nearest_ore(surface, position, ore_type)
 
     -- Check if the ore type exists
     if not resource then
-        DEBUG("No resource of type " .. ore_type .. " found.")
+        clog("No resource of type " .. ore_type .. " found.")
         return false
     end
 
@@ -33,7 +33,7 @@ function mine_resource(ore_type, quantity)
         if player.character.mining_state.mining then
             mined_quantity = mined_quantity + resource.amount
         else
-            DEBUG("Mining was unsuccessful.")
+            clog("Mining was unsuccessful.")
             return false
         end
     end
@@ -41,3 +41,5 @@ function mine_resource(ore_type, quantity)
     -- If the mining process was successful, return true
     return true
 end
+
+return mine_resource
