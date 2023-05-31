2be2ad9fce4bb8076ea2e8e5122c9d79ecda1e4e
willjallen <wallen2268@gmail.com>
2023-05-30 20:41:56 -0500

    - Logging statements now done with clog() - Set global message id per load or execute script call

--- foyager/mods/agent-actions/scripts.lua
+++ foyager/mods/agent-actions/scripts.lua
@@ -5,32 +5,41 @@ function print_player(msg)
     game.players[1].print(msg)
 end
 
--- Print log to file with msg_id attached for easier scraping
-function id_log(msg_id, msg)
-    log("Message ID: " .. msg_id .. " " ..msg)
+
+
+-- Custom log
+-- Print log to file with message_id attached for easier scraping
+function clog(msg)
+    log("Message ID: " .. global["message_id"] .. " " ..msg)
 end
 
 
 -- Loads in a script encoded in base64, saves it to the global context
 local function load_script(msg_id, function_name, script)
+    -- Set the message id
+    global["message_id"] = msg_id
+
+    -- game.players[1].print(msg_id)
+    -- game.players[1].print(global["message_id"])
+
     -- Decode the script string
 	local script_string = base64.decode(script)
     local loaded_chunk, err_msg = loadstring(script_string)
     
     -- If there's an error during loading, log it and return
     if loaded_chunk == nil then
-        id_log(msg_id, "Error loading script" .. function_name .. ": " .. err_msg)
+        clog("Error loading script" .. function_name .. ": " .. err_msg)
         return
     end
     
-    id_log(msg_id, "Script " .. function_name .. " loaded successfully")
+    clog("Script " .. function_name .. " loaded successfully")
 
     -- Try running the function and catching any runtime errors
     local ok, script_fn = pcall(loaded_chunk)
 
     -- If there's a runtime error, log it and return
     if not ok then
-        id_log(msg_id, "Error running script " .. function_name .. ": " .. script_fn)  -- script_fn contains error message in this case
+        clog("Error running script " .. function_name .. ": " .. script_fn)  -- script_fn contains error message in this case
         return
     end
 
@@ -39,15 +48,18 @@ end
 
 -- Executes a script
 local function execute_script(msg_id, function_name, ...)
+    -- Set the message id
+    global["message_id"] = msg_id
+
     if _G[function_name] then
         local ok, err = pcall(_G[function_name], ...)
         if not ok then
-            id_log(msg_id, "Error executing script " .. function_name .. ": " .. err)
+            clog("Error executing script " .. function_name .. ": " .. err)
         else
-            id_log(msg_id, "Script " .. function_name .. " executed successfully")
+            clog("Script " .. function_name .. " executed successfully")
         end
     else
-        id_log(msg_id, "Error: Function " .. function_name .. " does not exist.")
+        clog("Error: Function " .. function_name .. " does not exist.")
     end
 end
 
