b5b10d954f051ee061e13a4306a0c1cdf0f70bed
willjallen <wallen2268@gmail.com>
2023-06-01 02:22:07 -0500

    First attempt at an rLua parser

---
+++ foyager/env/rLua.py
@@ -0,0 +1,165 @@
+import re
+import uuid
+
+control_primitive_script = \
+'''
+function mine_resource(ore_type, quantity)
+    -- Check if arguments are valid
+    if type(ore_type) ~= 'string' or type(quantity) ~= 'number' then
+        clog("Invalid arguments. 'ore_type' should be a string and 'quantity' should be a number.")
+        return false
+    end
+
+  
+    -- Check if the ore type exists
+    if not resource then
+        clog("No resource of type " .. ore_type .. " found.")
+        return false
+    end
+
+    local player = game.players[1]
+
+    -- Check if the player is in a valid state
+    if not player.character or player.character.mining_state.mining then
+        clog("Player is not in a valid state for mining.")
+        return false
+    end
+
+    -- Find nearest ore entity
+    local position = player.character.position
+    local surface = player.surface
+    local resource_position = find_nearest_ore(surface, position, ore_type)
+
+    -- Move to ore position
+    await move(resource_position)
+
+    -- Mining loop
+    await mine(resouce_position, quantity)
+
+    -- If the mining process was successful, return true
+    return true
+end
+
+return mine_resource
+'''
+
+
+event_function_open_str = \
+'''
+script.on_event({event_id}, function(event)
+'''
+
+
+set_global_table_str = \
+'''
+    {timeline}
+'''
+
+EVENT_SUCCESS = 'event_success'
+EVENT_FAIL = 'event_fail'
+
+# Unique event names generator
+def generate_event_name():
+    return f"event_{uuid.uuid4().hex}"
+    
+
+def compile_to_rlua(source_lua):
+
+    # Global table of timelines
+    timelines = {}
+
+
+    # Find the last occurrence of "end"
+    last_end_index = source_lua.rfind("end")
+
+    # If "end" was found, slice the string up to and including that point
+    if last_end_index != -1:
+        truncated_source_lua = source_lua[:last_end_index]
+        
+    # Tokenize into lines
+    lines = truncated_source_lua.split("\n")
+        
+    # List to store the new code
+    output = []
+    
+    first_function = True
+    
+    current_event = generate_event_name()
+    for line in lines:
+        print('====================')
+        print('line ', line)
+        for l in output:
+            print(l)
+            print('--------------')
+        
+        # If comment, append and move on
+        comment_kwd = re.search(r'--\s+([a-zA-Z_][a-zA-Z_0-9]*)', line)   
+        
+        if comment_kwd: 
+            output.append(line)
+            continue
+            
+        # Search for the await keyword
+        await_kwd = re.search(r'await\s+([a-zA-Z_][a-zA-Z_0-9]*)', line)
+        
+        # Search for the return keyword
+        return_kwd = re.search(r'return\s+([a-zA-Z_][a-zA-Z_0-9]*)', line)
+        
+        if await_kwd:
+            # Append the command without await
+            output.append(re.sub(r'(\s*)await(\s*)', r'\1\2', line))
+            
+            # Close current event and start a new one
+            if first_function:
+                output.append(f"end  -- end of {current_event}")
+            else:
+                output.append(f"end)  -- end of {current_event}")
+                
+            # Generate new event name
+            next_event = generate_event_name()
+
+            # Update timelines
+            timelines[current_event] = next_event
+
+            # Start a new event
+            output.append(event_function_open_str.format(event_id=next_event))
+            
+            current_event = next_event
+            
+            if first_function: first_function = False
+        elif return_kwd:
+            true_kwd = re.search(r'return\s+([a-zA-Z_][a-zA-Z_0-9]*)', line)
+            false_kwd = re.search(r'return\s+([a-zA-Z_][a-zA-Z_0-9]*)', line)
+            function_name_kwd = re.search(r'dickface\s+([a-zA-Z_][a-zA-Z_0-9]*)', line)
+            
+            if true_kwd:
+                output.append(re.sub(r'(\s*)return(\s*)true', r'\1raise_event({})\2'.format(EVENT_SUCCESS), line))
+            elif false_kwd:
+                output.append(re.sub(r'(\s*)return(\s*)false', r'\1raise_event({})\2'.format(EVENT_FAIL), line))
+            elif function_name_kwd:
+                # Remove line
+                pass
+            
+        else:
+            # Regular line, just append to the output
+            output.append(line)
+        
+        # input()
+
+    # Close the last event
+    output.append(f"end)  -- end of {current_event}")
+
+    # Add the global table to the output
+    function_table_ptr_str = ''
+    for node in timelines:
+        function_table_ptr_str += 'global.function_table_ptr[{node}] = {connection}'.format(
+            node = node, 
+            connection = timelines[node]
+        )+'\n'
+         
+    output.insert(0, function_table_ptr_str)
+
+    return "\n".join(output)
+
+source_rlua = compile_to_rlua(control_primitive_script)
+print(source_rlua)
